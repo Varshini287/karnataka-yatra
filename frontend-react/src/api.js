2d61ffc78ff9bd5df7395ddcc84fdff9fb9c797c
Varshini287 <varshiniha2003@gmail.com>
2026-07-19 18:51:07 +0530

    Point frontend to deployed backend

--- frontend-react/src/api.js
+++ frontend-react/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000";
+const API_BASE = "https://karnataka-yatra.onrender.com";
 
 export async function getLevels() {
   const res = await fetch(`${API_BASE}/levels`);
